c537aaf8cf57490c3a4a77bccf0b0593c3e4a127
michelle :) <michelleeliasflores@gmail.com>
2023-08-15 13:08:21 -0400

    wave5CodeAlong <3333

--- githublesson/ContentView.swift
+++ githublesson/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("Thisismedemoing")
         }
         .padding()
     }

eeccccd80f9447b39078da76c29808f366d8a3e7
michelle :) <michelleeliasflores@gmail.com>
2023-08-15 12:53:48 -0400

    Initial Commit

---
+++ githublesson/githublessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  githublessonApp.swift
+//  githublesson
+//
+//  Created by Michelle Elias Flores on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct githublessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
